6d0f6967897426f9c050c5ee7e902fccdeaa4429
Sachin Singh <sachin8174056@gmail.com>
2025-08-30 20:24:50 +0530

    self.js

---
+++ datatyp02.js
@@ -0,0 +1,38 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello !!") we are using node js not browser
+
+// console.log(3+3);
+// console.log("Sachin");  code readibilty should be high
+
+// documentation tc39 & mdn
+
+let name = "Sachin"
+let age = 23;
+let isLoggedIn = false;
+let state;
+
+// Premitive datatype
+// Number => 2 to power 53;
+// bigint 
+// string
+// boolean
+// null => standalone value => typeof object
+// undefined => the value was not defined
+// symbol => for the unique
+
+
+// non-premitive datatype
+// array = typeof object
+// object
+// function
+
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefine
+
+
+
+
+
+
