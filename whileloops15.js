8befec7c3ceda30091fba2b244876a827d64a4f2
Sachin Singh <sachin8174056@gmail.com>
2025-09-04 08:29:17 +0530

    jspractices

---
+++ whileloops15.js
@@ -0,0 +1,45 @@
+// let i = 0
+// while (i <= 10) {
+//     console.log(`value of index is ${i}`);
+//     i = i + 2
+    
+// } 
+
+// let myArray = ["ironman", "hulk", "superman", "hanuman"]
+
+// let arr = 0;
+// while (arr < myArray.length) {
+//     console.log(`value is ${myArray[arr]}`);
+//     arr = arr + 1;
+// }
+
+
+// const arr = [1,2,3,4,5]
+
+// for(const num of arr) {
+//     console.log(num);
+    
+// }
+
+// const greetings = "Hello World !"
+// for (const greet of greetings){
+//     console.log(`each char is ${greet}`);
+    
+// }
+
+
+// maps 
+
+const map = new Map()
+map.set('IN', "INDIA")
+map.set('USA', "United State Of America")
+map.set('FR', "France")
+
+// console.log(map);
+
+for (const [key, value] of map){
+    console.log(key,":-" ,value);
+    
+}
+
+
