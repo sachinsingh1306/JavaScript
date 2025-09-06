3218ea82a94fded55a7e6fab84772d642b8c4454
Sachin Singh <sachin8174056@gmail.com>
2025-09-06 18:41:10 +0530

    js-loop

---
+++ lp.js
@@ -0,0 +1,38 @@
+// Question 1.
+// Multiplication Table
+// Write a program to print the multiplication table of a given number using a loop.
+
+// let num = 2;
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(`${num} X ${i} = ${num * i}`);
+// }
+
+// Question 2.
+// Factorial of a Number
+// Calculate the factorial of a given number using a for loop.
+
+// let n = 5;
+// let factorial = 1;
+
+// for (let i = 1; i <= n; i++) {
+//   factorial = factorial * i;
+// }
+// console.log(`Factorial of ${n} = ${factorial}`);
+
+
+// Question 3.
+// Sum of Natural Numbers
+// Find the sum of the first n natural numbers using a loop.
+
+let n = 12;
+let sum = 0;
+
+for (let i = 1; i <= n; i++){
+     sum = sum + i;
+}
+// console.log(sum);
+
+//Question 4.
+//Reverse a Number
+// Write a program to reverse the digits of a number using a loop.
