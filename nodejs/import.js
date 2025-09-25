bbe0b45cc832f83cb3b2a11c1e5c8b7e56202a26
Sachin Singh <sachin8174056@gmail.com>
2025-09-25 16:10:38 +0530

    update

---
+++ nodejs/import.js
@@ -0,0 +1,18 @@
+// import { sayHi, add, substract, multiply, divide } from "./index.js";
+
+// console.log(sayHi("Sachin"));
+// console.log(add(2,3));
+// console.log(substract(10,5));
+// console.log(multiply(10,5));
+// console.log(divide(10,2));
+
+
+import operation from "./index.js";
+
+console.log(operation.sayHi("Sachin"));
+console.log(operation.add(2,3));
+console.log(operation.substract(10,5));
+console.log(operation.multiply(10,5));
+console.log(operation.divide(10,2));
+
+
---
+++ nodejs/index.js
@@ -0,0 +1,24 @@
+function sayHi (name) {
+    return `Hi ${name}`
+}
+
+function add (a, b){
+    return a + b;
+}
+
+function substract (a, b){
+    return a - b;
+}
+
+function multiply (a , b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+// named export
+
+export {sayHi, add, substract, multiply, divide};
+
